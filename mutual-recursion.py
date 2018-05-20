a849a37f6a0f5cefdb43de18cf4b30108bff515a
dogac00 <ndakyildiz@gmail.com>
2018-05-20 21:15:37 +0300

    Update mutual-recursion.py

--- mutual-recursion.py
+++ mutual-recursion.py
@@ -20,3 +20,9 @@ def m(n):
         return 0
     else:
         return n - f(m(n-1))
+    
+# Another solution which is shorter can be:
+
+def f(n): return n - m(f(n-1)) if n else 1
+
+def m(n): return n - f(m(n-1)) if n else 0
