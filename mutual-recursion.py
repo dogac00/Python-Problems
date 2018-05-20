18632df76cfdbe073162acaa5ea97772cec5b931
dogac00 <ndakyildiz@gmail.com>
2018-05-20 21:14:05 +0300

    Create mutual-recursion.py

---
+++ mutual-recursion.py
@@ -0,0 +1,22 @@
+# Mutual Recursion allows us to take the fun of regular recursion (where a function calls itself until a terminating condition) 
+# and apply it to multiple functions calling each other!
+
+# Let's use the Hofstadter Female and Male sequences to demonstrate this technique. You'll want to create two functions F and M 
+# such that the following equations are true:
+
+# F(0) = 1
+# M(0) = 0
+# F(n) = n - M(F(n - 1))
+# M(n) = n - F(M(n - 1))
+
+def f(n):
+    if n == 0:
+        return 1
+    else:
+        return n - m(f(n-1))
+
+def m(n):
+    if n == 0:
+        return 0
+    else:
+        return n - f(m(n-1))
